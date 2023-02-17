2353c8063c8cf3bfc5be06bb0e8546fb0dba5212
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-17 14:43:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Aerosmith', 'The Rolling Stones'];
+
+const articles = ['a', 'an', 'the'];
+
+// Remove articles from the beginning of band names
+bandNames = bandNames.map((name) => {
+  let words = name.split(' ');
+  if (articles.includes(words[0].toLowerCase())) {
+    words.shift();
+  }
+  return words.join(' ');
+});
+
+// Sort the band names in lexicographic order
+bandNames.sort();
+
+// Add the sorted band names to the list in the HTML document
+const bandList = document.getElementById('band');
+bandNames.forEach((name) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = name;
+  bandList.appendChild(listItem);
+});
 
